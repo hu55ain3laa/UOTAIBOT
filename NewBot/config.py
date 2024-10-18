0b36c6f0181562fced8d0f9b6d52602d2e3056d1
Hussein Alaa <106111576+hu55ain3laa@users.noreply.github.com>
2024-10-18 17:13:21 +0300

    Delete NewBot/config.py

--- NewBot/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = f"mysql+pymysql://avnadmin:AVNS_2QjoUe49JiWOaAc3_Pt@mysql84-unique.h.aivencloud.com:23839/defaultdb?"
-USE_GPU = os.getenv("USE_GPU", "False").lower() == "true"
