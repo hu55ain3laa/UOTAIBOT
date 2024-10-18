b403bc9a2743e5e02820d50e9deb6a83b36fd679
Hussein Alaa <106111576+hu55ain3laa@users.noreply.github.com>
2024-10-18 17:08:41 +0300

    x

--- NewBot/config.py
+++ NewBot/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"mysql+pymysql://avnadmin:AVNS_2QjoUe49JiWOaAc3_Pt@mysql84-unique.h.aivencloud.com:23839/defaultdb?"
 USE_GPU = os.getenv("USE_GPU", "False").lower() == "true"
--- NewBot/database.py
+++ NewBot/database.py
@@ -1,7 +1,8 @@
 from sqlmodel import SQLModel, create_engine
 import os
+from config import DATABASE_URL
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///university_bot.db")
+
 engine = create_engine(DATABASE_URL)
 
 def init_db():
